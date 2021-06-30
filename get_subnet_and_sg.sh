8a846ab39e1576536884ba8f07f2e24b50de509c
sebastian cifuentes <secifuentes@deloitte.pt>
2021-06-30 16:57:16 +0100

    describe only instance with state running

--- get_subnet_and_sg.sh
+++ get_subnet_and_sg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-instance=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$APP_NAME-$BITBUCKET_DEPLOYMENT_ENVIRONMENT-web | jq .Reservations[0].Instances[0])
+instance=$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running Name=tag:Name,Values=$APP_NAME-$BITBUCKET_DEPLOYMENT_ENVIRONMENT-web | jq .Reservations[0].Instances[0])
 
 export SUBNET=$( jq .SubnetId <<< "${instance}" )
 export SG=$( jq -c '.SecurityGroups[] | select(.GroupName | contains("Web")) | .GroupId' <<< "${instance}" )
